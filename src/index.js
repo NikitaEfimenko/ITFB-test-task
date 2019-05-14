1f729b5786ed50c5c07c23e9f044dfe8eb6a1731
petruha2001 <petyag2001@gmail.com>
2019-05-14 12:32:56 +0300

    d
    d

--- src/index.js
+++ src/index.js
@@ -1,11 +1,84 @@
-class HelloWorld {
-  message = 'Hello World';
-  print = () => this.message;
+var sas = [];
+var d = 5;
+nextmas(3);
+setTimeout(upd, 300);
+upd();
+
+nextmas(20);
+setTimeout(upd, 1000);
+upd();
+prev.onclick = function () {
+  if (d == 4){
+    console.log(d);
+    prevmas(10);
+
+    setTimeout(upd, 1000);
+  }
+    d -= 1;
+    upd();
+}
+next.onclick = function () {
+    if (d == sas.length - 3){
+      nextmas(10);
+      console.log(d);
+      setTimeout(upd, 1000);
+    }
+    d += 1;
+    upd();
 }
 
-const hello = new HelloWorld();
-// eslint-disable-next-line no-console
-console.log(
-  `%c ${hello.print()}`,
-  `color: green; font-size:48px; weight: bold`,
-);
+
+
+function nextmas(num) {
+  list = document.getElementById("qwer");
+
+  for (var i = 0; i < num; i++){
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://picsum.photos/400/172', true);
+  xhr.responseType = 'blob';
+
+  xhr.onload = function(e) {
+  if (this.status == 200) {
+    var blob = this.response;
+    sas.push(window.URL.createObjectURL(blob));
+
+  }
+  };
+
+  xhr.send();
+
+}
+
+}
+
+function prevmas(num) {
+  list = document.getElementById("qwer");
+  for (var i = 0; i < num; i++){
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://picsum.photos/400/172', true);
+  xhr.responseType = 'blob';
+
+  xhr.onload = function(e) {
+  if (this.status == 200) {
+    var blob = this.response;
+    sas.unshift(window.URL.createObjectURL(blob));
+
+
+
+  }
+  };
+
+  xhr.send();
+
+}
+d += 10;
+}
+
+
+function upd() {
+console.log(d);
+rigthimg.src = sas[d + 1];
+midimg.src = sas[d];
+leftimg.src = sas[d - 1];
+
+}
